740dfa3f1ddcd8ddfef261d5379f82543ecb0a56
Diego Coelho Lins <105721945+diegouidev@users.noreply.github.com>
2024-04-08 23:54:01 +0000

    alterando logomarca

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,45 @@
-import React, { useState } from 'react';
-import html2canvas from 'html2canvas';
-import jsPDF from 'jspdf';
+import { useState } from "react";
+import html2canvas from "html2canvas";
+import jsPDF from "jspdf";
 import { Header } from "./components/header";
 
 export function App() {
-  const [valor, setValor] = useState('');
+  const [valor, setValor] = useState("");
   const [parcelas, setParcelas] = useState([]);
 
   // Função para formatar o valor para o formato de moeda brasileira (R$)
   const formatarValor = (value) => {
-    const floatValue = parseFloat(value.replace(/[^\d]/g, '')) / 100;
-    if (isNaN(floatValue)) return '';
-    return `R$ ${floatValue.toFixed(2).replace('.', ',').replace(/\B(?=(\d{3})+(?!\d))/g, '.')}`;
+    const floatValue = parseFloat(value.replace(/[^\d]/g, "")) / 100;
+    if (isNaN(floatValue)) return "";
+    return `R$ ${floatValue
+      .toFixed(2)
+      .replace(".", ",")
+      .replace(/\B(?=(\d{3})+(?!\d))/g, ".")}`;
   };
 
   // Função para calcular as parcelas
   const calcularParcelas = (value) => {
-    const valorFloat = parseFloat(value.replace(/[^\d]/g, '')) / 100;
+    const valorFloat = parseFloat(value.replace(/[^\d]/g, "")) / 100;
     if (isNaN(valorFloat)) return [];
-    const jurosPorParcela = [1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0, 5.5, 6.0, 6.5, 7.0];
+    const jurosPorParcela = [
+      1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0, 5.5, 6.0, 6.5, 7.0,
+    ];
     const parcelasCalculadas = [];
-    let valorRestante = valorFloat;
+    // let valorRestante = valorFloat;
     for (let i = 1; i <= 12; i++) {
-      const valorPorParcela = valorFloat / i * (1 + jurosPorParcela[i - 1] / 100);
+      const valorPorParcela =
+        (valorFloat / i) * (1 + jurosPorParcela[i - 1] / 100);
       parcelasCalculadas.push({
         parcela: i,
-        valorParcela: `R$ ${(valorPorParcela).toFixed(2).replace('.', ',').replace(/\B(?=(\d{3})+(?!\d))/g, '.')}`,
-        valorTotal: `R$ ${(valorPorParcela * i).toFixed(2).replace('.', ',').replace(/\B(?=(\d{3})+(?!\d))/g, '.')}`,
-        juros: jurosPorParcela[i - 1]
+        valorParcela: `R$ ${valorPorParcela
+          .toFixed(2)
+          .replace(".", ",")
+          .replace(/\B(?=(\d{3})+(?!\d))/g, ".")}`,
+        valorTotal: `R$ ${(valorPorParcela * i)
+          .toFixed(2)
+          .replace(".", ",")
+          .replace(/\B(?=(\d{3})+(?!\d))/g, ".")}`,
+        juros: jurosPorParcela[i - 1],
       });
     }
     return parcelasCalculadas;
@@ -40,32 +52,43 @@ export function App() {
   };
 
   const compartilharViaWhatsApp = () => {
-    const mensagem = `Confira o detalhamento do parcelamento:\n\n${parcelas.map((parcela) => `Parcela ${parcela.parcela}: ${parcela.valorParcela} - Valor Total: ${parcela.valorTotal}`).join('\n')}`;
+    const mensagem = `Confira o detalhamento do parcelamento:\n\n${parcelas
+      .map(
+        (parcela) =>
+          `Parcela ${parcela.parcela}: ${parcela.valorParcela} - Valor Total: ${parcela.valorTotal}`
+      )
+      .join("\n")}`;
     const link = `https://wa.me/?text=${encodeURIComponent(mensagem)}`;
-    window.open(link, '_blank');
+    window.open(link, "_blank");
   };
 
   const gerarPDF = () => {
-    const input = document.getElementById('tabela-parcelas');
-    html2canvas(input)
-      .then((canvas) => {
-        const imgData = canvas.toDataURL('image/png');
-        const pdf = new jsPDF();
-        pdf.addImage(imgData, 'PNG', 0, 0);
-        pdf.save('detalhamento_parcelamento.pdf');
-      });
+    const input = document.getElementById("tabela-parcelas");
+    html2canvas(input).then((canvas) => {
+      const imgData = canvas.toDataURL("image/png");
+      const pdf = new jsPDF();
+      pdf.addImage(imgData, "PNG", 0, 0);
+      pdf.save("detalhamento_parcelamento.pdf");
+    });
   };
 
   return (
     <div className="mx-auto flex flex-col items-center h-screen bg-gray-200">
       <Header />
-      <h1 className="text-3xl font-semibold mb-8 mt-8">Simulador de Parcelamento</h1>
-      <div class="max-w-3xl p-6 bg-white border border-gray-100 rounded-lg shadow dark:bg-gray-100 dark:border-gray-200">
-        <h2 className="text-3xl- font-semibold mb-6">Faça sua simulação aqui</h2>
+      <h1 className="text-3xl font-semibold mb-8 mt-8">
+        Simulador de Parcelamento
+      </h1>
+      <div className="max-w-3xl p-6 bg-white border border-gray-100 rounded-lg shadow dark:bg-gray-100 dark:border-gray-200">
+        <h2 className="text-3xl- font-semibold mb-6">
+          Faça sua simulação aqui
+        </h2>
 
         <div className="max-w-md">
           <div className="mb-4">
-            <label htmlFor="valor" className="block text-sm font-medium text-gray-700">
+            <label
+              htmlFor="valor"
+              className="block text-sm font-medium text-gray-700"
+            >
               Valor Total:
             </label>
             <input
@@ -81,7 +104,9 @@ export function App() {
         </div>
 
         <div className="flex flex-col items-center mt-8">
-          <h2 className="text-xl font-semibold mb-4">Detalhes do Parcelamento:</h2>
+          <h2 className="text-xl font-semibold mb-4">
+            Detalhes do Parcelamento:
+          </h2>
           <table id="tabela-parcelas" className="table-auto">
             <thead>
               <tr>
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,9 +1,12 @@
-import logo from '../assets/logo.jpeg'
+// import logo from '../assets/logo.jpeg'
 
 export function Header() {
-    return (
-        <div className='flex items-center gap-5 py-2 w-full bg-zinc-900 justify-center'>
-           <img src={logo} alt="logo nlw" className='size-24' />
-        </div>
-    )
+  return (
+    <div className="flex items-center gap-5 py-2 w-full bg-zinc-900 justify-center">
+      <h1 className="mb-4 mt-4 text-2xl font-extrabold leading-none tracking-tight text-gray-900 md:text-3xl lg:text-3xl dark:text-white">
+        SHOPNET BR
+      </h1>
+      {/* <img src={logo} alt="logo nlw" className='size-24' /> */}
+    </div>
+  );
 }
