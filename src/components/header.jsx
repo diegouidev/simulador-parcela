12ae2c64bc7956dc7cb5026b88ac4dfcd2ff4bc7
Diego Coelho Lins <105721945+diegouidev@users.noreply.github.com>
2024-04-11 13:09:43 +0000

    update logomarca

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,12 +1,12 @@
-// import logo from '../assets/logo.jpeg'
+import logo from "../assets/logo-cloud-branca.png";
 
 export function Header() {
   return (
     <div className="flex items-center gap-5 py-2 w-full bg-zinc-900 justify-center">
-      <h1 className="mb-4 mt-4 text-2xl font-extrabold leading-none tracking-tight md:text-3xl lg:text-3xl dark:text-white">
+      {/* <h1 className="mb-4 mt-4 text-2xl font-extrabold leading-none tracking-tight md:text-3xl lg:text-3xl dark:text-white">
         CLOUD DESIGN
-      </h1>
-      {/* <img src={logo} alt="logo nlw" className='size-24' /> */}
+      </h1> */}
+      <img src={logo} alt="logo cloud design" className="h-18 w-96 my-2" />
     </div>
   );
 }
